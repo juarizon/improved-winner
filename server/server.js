4535cc97286eb97ae6c9ba772cc23f969ceadf19
Juari Ritter Müller <juarirm@juarizon.com.br>
2018-09-10 16:21:47 -0300

    Refatorado mongoose em módulos e adicionada rota que adiciona via POST

--- server/server.js
+++ server/server.js
@@ -1,37 +1,24 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-var Todo = mongoose.model('Todos', {
-  text: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  },
-  completedAt: {
-    type: Number
-  }
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then( (doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.send(e);
+  });
 });
 
-// var newTodo = new Todo({
-//   text: "Cook dinner"
-// });
-
-// newTodo.save().then( (doc) => {
-//   console.log('Saved todo',doc);
-// }, (e) => {
-//   console.log('Unable to save todo',e);
-// })
-
-var completedAt = new Date('2018-09-01 12:00').valueOf();
-var newTodo = new Todo({
-  text: "Cook dinner",
-  completed: true,
-  completedAt
+app.listen(3000, () => {
+  console.log('Started on port 3000')
 });
-newTodo.save().then( (doc) => {
-  console.log('Saved todo',doc);
-}, (e) => {
-  console.log('Unable to save todo',e);
-})
