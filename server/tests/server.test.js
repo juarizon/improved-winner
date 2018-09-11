0e4ef4a1da7503d4d1d88ed4c33b67793da523f2
Juari Ritter Müller <juarirm@juarizon.com.br>
2018-09-11 11:00:05 -0300

    Teste GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,22 @@ const expect = require('expect');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 const {User} = require('./../models/user');
+const todos = [{
+  text: "Cook dinner"
+},{
+  text: "Alguma coisa a fazer do Postman"
+},{
+  text: "Passear nos setores"
+},{
+  text: "Revisar moto"
+}];
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then( () => {
+    Todo.insertMany(todos).then( () => done());
+  });
 });
+
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
     var text = 'Text todo text';
@@ -15,13 +28,13 @@ describe('POST /todos', () => {
       .send({text})
       .expect(200)
       .expect((res) => {
-        expect(res.body.text).toBe(text+'');
+        expect(res.body.text).toBe(text);
       })
-      .end((err, res)=>{
+      .end((err, res) => {
         if(err){
           return done(err);
         }
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -38,11 +51,23 @@ describe('POST /todos', () => {
         if(err){
           return done(err)
         }
-        
+
         Todo.find().then( (todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(4);
           done();
         }).catch( (e) => done(e));
       })
   });
 });
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect( (res) => {
+        expect(res.body.todos.length).toBe(4);
+      })
+      .end(done);
+  })
+})
